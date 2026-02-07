a7b68fbbf9746cd1e1b087b35119f6adb80ec7de
Josingp <tjddbs0803@gmail.com>
2026-02-07 21:32:41 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,9 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
+    // .env 파일 로드
+    const env = loadEnv(mode, process.cwd(), '');
+    
     return {
       server: {
         port: 3000,
@@ -11,8 +13,7 @@ export default defineConfig(({ mode }) => {
       },
       plugins: [react()],
       define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+        // [수정] 불필요한 키 삭제하고 오직 TMAP 키만 연결
         'process.env.VITE_TMAP_APP_KEY': JSON.stringify(env.VITE_TMAP_APP_KEY)
       },
       resolve: {
